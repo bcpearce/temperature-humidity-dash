de04b3fc6a34a2b4227ce3cb9475de424e0d531a
Benjamin Pearce <hawk.git@bcpearce.com>
2015-08-04 18:38:54 -0400

    updated styling and added humidity

--- app/assets/javascripts/api/v1/readings.js
+++ app/assets/javascripts/api/v1/readings.js
@@ -8,25 +8,40 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
 
   var tJSON = $.getJSON( "/api/v1/readings.json", function(data) {
-    var vizData = [['Time', 'Temperature']];
-    var dataLength = data.length
+
+    var vizData = [['Time', 'Temperature', 'Humidity']];
+    var dataLength = data.length;
+
+    if (dataLength <= 0) {
+      return
+    }
 
     for (var i = 0; i < dataLength; i++) {
       date = new Date(data[i].datetime);
-      temp = parseFloat(data[i].temperature)
-      vizData.push([date, temp]);
+      temp = parseFloat(data[i].temperature);
+      hum = parseFloat(data[i].humidity);
+      vizData.push([date, temp, hum]);
     }
 
     var tableData = google.visualization.arrayToDataTable(vizData);
 
     var options = {
-      title: 'Temperature (F)',
+      title: ['Temperature (F)', 'Humdity (%)'],
       titleTextStyle: {color: 'white'},
       hAxis: {title: 'Time',  titleTextStyle: {color: '#FFF'},
               textStyle: {color: '#666'}},
-      vAxis: {minValue: 40, textStyle: {color: '#666'}},
+      vAxis: {textStyle: {color: '#666'},
+              titleTextStyle: {color: '#FFF'}},
+      series: {
+        0: {targetAxisIndex: 0},
+        1: {targetAxisIndex: 1}
+      },
+      vAxes: {
+        0: {title: 'Temps (Fahrenheit)'},
+        1: {title: 'Humidity (%)'}
+      },
       backgroundColor: {fill:'#222', stroke:'#aaa'},
-      colors: ['yellow'],
+      colors: ['yellow', 'blue'],
       areaOpacity: 0.12
     };
 
