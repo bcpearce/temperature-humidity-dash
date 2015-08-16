0801aa9699d5415bedc1686367337aa40960682e
Benjamin Pearce <hawk.git@bcpearce.com>
2015-08-16 12:33:28 -0400

    added rounding to measurments

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,2 +1,10 @@
 class Reading < ActiveRecord::Base
+
+  before_save :round_measurements
+
+  private
+    def round_measurements
+      self.temperature = self.temperature.round(1)
+      self.humidity = self.humidity.round(1)
+    end
 end
--- spec/models/reading_spec.rb
+++ spec/models/reading_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Reading, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "rounds any reading to 1 decimal place" do
+    r = Reading.create(temperature:40.1657, humidity:40.1657)
+    expect([r.temperature, r.humidity]).to eq([40.2, 40.2])
+  end
 end
